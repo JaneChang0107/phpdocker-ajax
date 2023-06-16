844698c0525d313fb66e3ca17fbd9130996b1679
JaneChang0107 <45963018+JaneChang0107@users.noreply.github.com>
2023-06-16 10:27:04 +0900

    mvc

---
+++ html/mvc/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+define("DB_DRIVER", "mysql");
+define("DB_HOST", "172.18.0.1");
+define("DB_USER", "test");
+define("DB_PASS", "test");
+define("DB_DATABASE", "test");
+define("DB_CHARSET", "utf8");
+/*
+return array(
+    "driver"    =>"mysql",
+    "host"      =>"localhost",
+    "user"      =>"root",
+    "pass"      =>"test",
+    "database"  =>"test",
+    "charset"   =>"utf8"
+);
+*/
+?>
