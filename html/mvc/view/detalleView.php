e58028dfbf7c87a0f9bf38338d5de0b499e678fc
EILEEN_CHANG\chiac <chiachen.chang@t8s.co.jp>
2023-06-16 19:21:55 +0900

    localstoragesave

--- html/mvc/view/detalleView.php
+++ html/mvc/view/detalleView.php
@@ -22,12 +22,12 @@
     </head>
     <body>
         <div class="col-lg-5 mr-auto">
-            <form action="index.php?controller=employees&action=actualizar" method="post">
+            <form action="index.php?controller=employees&action=actualizar" method="post" id="contact-form">
                 <h3>User detail</h3>
                 <hr/>
                 <input type="hidden" name="id" id="id" value="<?php echo $datos["employee"]->id ?>"/>
-                Name: <input type="text" name="Name" id="name" value="<?php echo $datos["employee"]->Name ?>" class="form-control"/>
-                Surname: <input type="text" name="Surname" id="surname" value="<?php echo $datos['employee']->Surname ?>" class="form-control"/>
+                Name: <input type="text" name="Name" id="Name" value="<?php echo $datos["employee"]->Name ?>" class="form-control"/>
+                Surname: <input type="text" name="Surname" id="Surname" value="<?php echo $datos['employee']->Surname ?>" class="form-control"/>
                 Email: <input type="text" name="email" id="email" value="<?php echo $datos['employee']->email ?>" class="form-control"/>
                 phone: <input type="text" name="phone"  id="phone" value="<?php echo$datos['employee']->phone ?>" class="form-control"/>
                 <input type="submit" value="Send" id="send" class="btn btn-success"/>
@@ -59,68 +59,115 @@
         // });
 
     $(document).ready(function(){
+//transform form content to json object
+        var form = $("#contact-form");
+        
+        var before = convertFormToJSON(form);
+        //console.log(before);
 
-		var phone = $("#phone").val();
-		
-        $("#phone").change(function() {
-            phone =  $(this).val();
-            setWithExpiry("phone", phone, 30000);
+        function convertFormToJSON(form) {
+            var array = $(form).serializeArray(); // Encodes the set of form elements as an array of names and values.
+            console.log(array);
+            var json = {};
+            $.each(array, function () {
+            json[this.name] = this.value || "";
+        });
+        return json;
+        }
+//set json object to local storage
+        //localStorage.setItem('testObject', JSON.stringify(before));
+//detect change
+
+        $("#contact-form").change(function() {
+            setChangedDataToLocalStorage();
         });
 
-		if(localStorage.getItem('phone') != null){
+        function setChangedDataToLocalStorage() {
+            $this = $("#contact-form");       
+            var after = convertFormToJSON($this);
+            if(before===after){
+                return;
+            }else{
+                //localStorage.setItem('testObject', JSON.stringify(after));
+                setWithExpiry('testObject', after, 50000)
+            }
+        };   
+        
+        if(localStorage.getItem('testObject') != null){
             var html=`<input type="button" value="retrieve" class="btn btn-danger"/>`
             $("#retrieve").html(html);
-        }
+        };
 
         $("#retrieve").click(function(){           
-            var dataFromLocalstorage = localStorage.getItem('phone');
-            console.log(dataFromLocalstorage);
-            $("#phone").val(dataFromLocalstorage);
+            var dataFromLocalstorage = localStorage.getItem('testObject');
+            var obj = JSON.parse(dataFromLocalstorage);  
+            for (const key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    console.log(`${key}: ${obj[key]}`);
+                    $("#"+key).val(obj[key]);
+                }              
+            }            
         });
+   
+    
+
+	// 	var phone = $("#phone").val();
 		
-        getWithExpiry("phone");
+    //     $("#phone").change(function() {
+    //         phone =  $(this).val();
+    //         setWithExpiry("phone", phone, 30000);
+    //     });
+
+	// 	if(localStorage.getItem('phone') != null){
+    //         var html=`<input type="button" value="retrieve" class="btn btn-danger"/>`
+    //         $("#retrieve").html(html);
+    //     }
+
+    //     $("#retrieve").click(function(){           
+    //         var dataFromLocalstorage = localStorage.getItem('phone');
+    //         console.log(dataFromLocalstorage);
+    //         $("#phone").val(dataFromLocalstorage);
+    //     });
+		
+        // getWithExpiry("testObject");
 
         function setWithExpiry(key, value, ttl) {
             const now = new Date()
             // `item` is an object which contains the original value
             // as well as the time when it's supposed to expire
             var expirytime=(now.getTime() + ttl).toString();
-            const item = {
-                value: value,
-                expiry: expirytime
-            }
-            //  let dd = new Date(now.getTime() + ttl);
-            //  console.log(dd.toString() + "<br />");            
-            localStorage.setItem(key, JSON.stringify(item))
+            var content = JSON.stringify(value);   
+            localStorage.setItem(key, content);
         }
 
-        function getWithExpiry(key) {
-			const itemStr = localStorage.getItem(key)
+        // function getWithExpiry(key) {
+		// 	const itemStr = localStorage.getItem(key)
 
-			// if the item doesn't exist, return null
-			if (!itemStr) {
-				return null;
-			}
+		// 	// if the item doesn't exist, return null
+		// 	if (!itemStr) {
+		// 		return null;
+		// 	}
 
-			const item = JSON.parse(itemStr)
-			const now = new Date()
+		// 	const item = JSON.parse(itemStr)
+		// 	const now = new Date()
 
-			// compare the expiry time of the item with the current time
-            alert (now.getTime());
-			if (now.getTime() > item.expiry) {
+		// 	// compare the expiry time of the item with the current time
+        //     alert (now.getTime());
+		// 	if (now.getTime() > item.expiry) {
                 
-				// If the item is expired, delete the item from storage
-				// and return null
-				localStorage.removeItem(key);
-                $("#retrieve").html("");
-				return null;
-			}
-			return item.value;
-		} 
+		// 		// If the item is expired, delete the item from storage
+		// 		// and return null
+		// 		localStorage.removeItem(key);
+        //         $("#retrieve").html("");
+		// 		return null;
+		// 	}
+		// 	return item.value;
+		// } 
 
-    });
+    // });
         $("#send").click(function(){
-            localStorage.removeItem('name');
+            localStorage.removeItem('testObject');
         });
+    });
     </script>
 </html>
